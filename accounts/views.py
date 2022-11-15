4ad52584e2653642ed1edd6079f5e4435b35721e
Kiash254 <codewithkiash@gmail.com>
2022-11-15 20:43:33 +0300

    added mail.html files

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from django.core.mail  import send_mail
+from django.conf import settings
 # Create your views here.
 
 def AccountView(request):
-    
+    if request.method== "POST":
+        subject=request.POST.get('subject')
+        message=request.POST.get('message')
+        email=request.POST.get('email')
+        send_mail(subject,message,email,settings.EMAIL_HOST_USER, [email],fail_silently=False)
+        return render(request,'accounts/sent.html',{'email':email}) 
     context={}
     return render(request,'accounts/mail.html')
--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,13 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#smpt configurations
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='codewithkiash@gmail.com'
+EMAIL_HOST_PASSWORD='kiash1234!@#$'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
